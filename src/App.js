dc889d9950b591924afbf28a339ad0381ca3799e
Md Nurul Islam <nurulcse09@gmail.com>
2023-01-26 23:01:41 +0600

    adding bootstrap in react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-warning">
+        <div className="container">
+          <a className="navbar-brand text-dark" href="/">
+            REACT
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav  me-auto mb-2 mx-auto mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link text-dark" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link text-dark" href="/">
+                  About
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link text-dark" href="/">
+                  Contact
+                </a>
+              </li>
+
+             
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control text-dark rounded-0 me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              ></input>
+              <button className="btn btn-info rounded-0 text-dark" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
