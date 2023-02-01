77c4b86fd8a5e7a0784b933e7a1a8db944fa020d
Md Nurul Islam <nurulcse09@gmail.com>
2023-02-01 23:02:42 +0600

    Home Page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import heroImg from '../assets/head.png'
 
 const Hero = () => {
+
+  const align_block = {
+    textAlign:"justify"
+  }
+  const navigate = {
+    position : "absolute",
+    top:'70%',
+    left:"25%"
+  }
   return (
     <>
       <div className='py-3'>
@@ -10,12 +20,26 @@ const Hero = () => {
             <div className="col-md-6">
               <img src={heroImg} alt="" className='w-100' />
             </div>
-            <div className="col-md-6">
-              csdvsd
+            <div className="col-md-6 pt-5">
+              <h3>We are a GEEK Squad</h3>
+              <p style={align_block}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis sint recusandae tenetur
+                 porro distinctio possimus aliquid mollitia enim quod similique labore architecto, 
+                 voluptates ratione ad autem temporibus sequi asperiores itaque explicabo assumenda 
+                 ipsam? Doloremque, at. Impedit reprehenderit aliquam, quibusdam tempora enim veniam 
+                 maxime deleniti soluta ipsa magnam dolorum <br/> <br/> suscipit labore expedita, doloremque ipsu
+                 m corrupti est in deserunt placeat vel dolore officiis. Aperiam animi delectus, a re
+                 pudiandae quo aut nesciunt temporibus, ipsam voluptate nulla nostrum recusandae dese
+                 runt odio sint adipisci? Recusandae iusto officia quod reprehenderit consequatur, cum 
+                 architecto accusamus ullam doloremque maiores illo et soluta quasi quos hic at labore 
+                 nulla amet perferendis eaqu</p>
             </div>
           </div>
-
         </div>
+      </div>
+
+      <div style={navigate}>
+        <Link to='/contact' className='btn btn-sm btn-success rounded-0 me-3'>Contact</Link>
+        <Link to='/project' className='btn btn-sm btn-warning rounded-0'>Project</Link>
       </div>
     </>
   )
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const About = () => {
+  const align_block = {
+    textAlign: "justify",
+  };
   return (
     <>
-      <Navbar/>
-    </>
-  )
-}
+      <Navbar />
+      <div className="py-3">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 pt-5">
+              <h3>We are a GEEK Squad</h3>
+              <p style={align_block}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis
+                sint recusandae tenetur porro distinctio possimus aliquid
+                mollitia enim quod similique labore architecto, voluptates
+                ratione ad autem temporibus sequi asperiores itaque explicabo
+                assumenda ipsam? Doloremque, at. Impedit reprehenderit aliquam,
+                quibusdam tempora enim veniam maxime deleniti soluta ipsa magnam
+                dolorum <br /> <br /> suscipit labore expedita, doloremque ipsu
+                m corrupti est in deserunt placeat vel dolore officiis. Aperiam
+                animi delectus, a re pudiandae quo aut nesciunt temporibus,
+                ipsam voluptate nulla nostrum recusandae dese runt odio sint
+                adipisci? Recusandae iusto officia quod reprehenderit
+                consequatur, cum architecto accusamus ullam doloremque maiores
+                illo et soluta quasi quos hic at labore nulla amet perferendis
+                eaqu
+              </p>
+            </div>
 
-export default About
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
