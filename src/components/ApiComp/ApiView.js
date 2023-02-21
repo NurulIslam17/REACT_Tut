591f145c1a7ec64537513e40ad25a8b14342755b
Md Nurul Islam <nurulcse09@gmail.com>
2023-02-21 13:03:52 +0600

    Adding Spinner

--- src/components/ApiComp/ApiView.js
+++ src/components/ApiComp/ApiView.js
@@ -2,18 +2,22 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Spinner from "../Spinner";
 
+// User API Links
 const getURL = "http://127.0.0.1:8000/api/get_user";
 const deleteURL = "http://127.0.0.1:8000/api/delete";
+// const storeURL = "http://127.0.0.1:8000/api/store_user";
 
 function ApiView() {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [deleteMsg, setDeleteMsg] = useState(false);
 
   // view data in table  (get data from database)
   const getData = () => {
+    
     fetch(getURL)
       .then((response) => {
+        setIsLoading(true);
         return response.json();
       })
       .then((data) => {
@@ -21,18 +25,22 @@ function ApiView() {
       });
   };
   useEffect(() => {
+    
     getData();
     setIsLoading(false);
+    
   }, []);
-
-  // delete use
+  // delete user
   const deleteHandle = (id) => {
-    fetch(deleteURL+`/${id}`,{ method:'DELETE'}).then((response) => {
+    fetch(deleteURL + `/${id}`, { method: "DELETE" }).then((response) => {
       if (!response.ok) {
         setDeleteMsg(false);
       }
       getData();
       setDeleteMsg(true);
+      setTimeout(() => {
+        setDeleteMsg(false);
+      }, 1500);
     });
   };
 
@@ -41,8 +49,8 @@ function ApiView() {
       <h4 className="text-center">User Info</h4>
       {deleteMsg && <p className="text-danger">Data Deleted</p>}
       
+
       <table className="table table-bordered table-striped">
-        
         <thead>
           <tr className="bg-info">
             <th scope="col">SL</th>
@@ -56,13 +64,11 @@ function ApiView() {
         </thead>
 
         <tbody>
-          
           {data &&
             data.map((value, index) => {
               const { id, name, email, phone } = value;
               return (
                 <>
-                {isLoading && <Spinner />}
                   <tr key={id}>
                     <th scope="row">{index + 1}</th>
                     <td>{name}</td>
@@ -74,9 +80,11 @@ function ApiView() {
                       </button>
                       <button
                         className="btn btn-sm btn-danger rounded-0 me-2"
-                        onClick={()=>{deleteHandle(id)}}
+                        onClick={() => {
+                          deleteHandle(id);
+                        }}
                       >
-                        Deletei
+                        Delete
                       </button>
                     </td>
                   </tr>
@@ -84,8 +92,9 @@ function ApiView() {
               );
             })}
         </tbody>
-
+        
       </table>
+      {!isLoading && <Spinner />}
     </>
   );
 }
